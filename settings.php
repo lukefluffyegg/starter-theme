d18140c1817a27df5d6b822902561e57fface112
Luke Davies <lukedavies@Lukes-MacBook.local>
2017-03-07 15:31:07 +0000

    Fixed settings page

--- settings.php
+++ settings.php
@@ -1,7 +1,5 @@
 <?php
 
-function theme_settings_page(){}
-
 function add_theme_menu_item()
 {
     add_menu_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
@@ -39,54 +37,6 @@ function display_facebook_element()
     <?php
 }
 
-function display_theme_panel_fields()
-{
-    add_settings_section("section", "All Settings", null, "theme-options");
-    
-    add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
-    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
-
-    register_setting("section", "twitter_url");
-    register_setting("section", "facebook_url");
-}
-
-add_action("admin_init", "display_theme_panel_fields");
-
-function display_twitter_element()
-{
-    ?>
-        <input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
-    <?php
-}
-
-function display_facebook_element()
-{
-    ?>
-        <input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
-    <?php
-}
-
-function display_layout_element()
-{
-    ?>
-        <input type="checkbox" name="theme_layout" value="1" <?php checked(1, get_option('theme_layout'), true); ?> /> 
-    <?php
-}
-
-function display_theme_panel_fields()
-{
-    add_settings_section("section", "All Settings", null, "theme-options");
-    
-    add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
-    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
-    add_settings_field("theme_layout", "Do you want the layout to be responsive?", "display_layout_element", "theme-options", "section");
-
-    register_setting("section", "twitter_url");
-    register_setting("section", "facebook_url");
-    register_setting("section", "theme_layout");
-}
-
-add_action("admin_init", "display_theme_panel_fields");
 
 function logo_display()
 {
@@ -111,10 +61,19 @@ function handle_logo_upload()
 function display_theme_panel_fields()
 {
     add_settings_section("section", "All Settings", null, "theme-options");
+
+    add_settings_field("twitter_url", "Twitter Profile Url", "display_twitter_element", "theme-options", "section");
+    add_settings_field("facebook_url", "Facebook Profile Url", "display_facebook_element", "theme-options", "section");
+
+    register_setting("section", "twitter_url");
+    register_setting("section", "facebook_url");
+
     
-    add_settings_field("logo", "Logo", "logo_display", "theme-options", "section");  
+    add_settings_field("logo", "Logo", "logo_display", "theme-options", "section");
 
     register_setting("section", "logo", "handle_logo_upload");
 }
 
 add_action("admin_init", "display_theme_panel_fields");
+
+?>
